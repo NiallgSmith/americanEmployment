8d776c4ad7dc3a2a3b55e4bd929cb516c7cab1df
Niall Smith <niallsmi@gmail.com>
2016-08-03 16:50:55 +0100

    finally someprogress

---
+++ static/js/basic.js
@@ -0,0 +1,15 @@
+/**
+ * Created by niallsmith on 03/08/16.
+ */
+
+$(document).ready(function(){
+    $("#hide").click(function () {
+        $("#header").hide("slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#show").click(function () {
+        $("#header").show("slow");
+    });
+});
